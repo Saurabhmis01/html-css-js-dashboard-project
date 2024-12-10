7914afb7ffa2e03642affcf53dc5b2eb0b041586
Saurabhmis01 <157217564+Saurabhmis01@users.noreply.github.com>
2024-12-10 18:40:54 +0530

    Add files via upload
    main

---
+++ admindashboard.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('#menu').click(function(){
+        $('aside').toggle()
+    })
+})
+
+$(document).ready(function(){
+    $('menu').click(function(){
+        $('aside').show()
+    })
+})
